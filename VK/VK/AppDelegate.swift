2f8c0e45d9d7c8f31c92ec2cfd8b7473be065c95
Михаил <40540053+egorovmikhail@users.noreply.github.com>
2020-03-25 01:51:37 +1100

    Add files via upload

---
+++ VK/VK/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  VK
+//
+//  Created by Михаил Егоров on 25.03.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+}
+
---
+++ VK/VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Михаил Егоров on 25.03.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
