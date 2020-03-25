f4d3401dfa20f2543093e8695ae8875b7c637362
egorovmikhail <egorovmikhail@bk.ru>
2020-03-26 04:19:12 +1100

    Допилил домашку № 1

---
+++ VK/VK/LoginFormController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginFormController.swift
+//  VK
+//
+//  Created by Михаил Егоров on 25.03.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+    
+    @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+
+    @IBAction func loginPressed() {
+        
+        let login = loginField.text!
+        let password = passwordField.text!
+                
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        scrollBottomConstraint.constant = frame.height
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        scrollBottomConstraint.constant = 0
+    }
+}
+
