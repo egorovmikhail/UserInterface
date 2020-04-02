02b03373597d0acfebe2218708874bf52cf0236e
egorovmikhail <egorovmikhail@bk.ru>
2020-04-03 00:08:18 +1100

    добавил отображение друзей и создал файлы для структур

---
+++ VK/VK/Model/ModelUser.swift
@@ -0,0 +1,10 @@
+//
+//  ModelUser.swift
+//  VK
+//
+//  Created by Михаил Егоров on 02.04.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import Foundation
+
---
+++ VK/VK/Model/Model​Group.swift
@@ -0,0 +1,9 @@
+//
+//  Model​Group.swift
+//  VK
+//
+//  Created by Михаил Егоров on 03.04.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import Foundation
