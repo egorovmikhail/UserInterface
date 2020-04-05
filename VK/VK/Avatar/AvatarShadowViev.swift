58a550a7c53f3a4655f3b75be3fc1a9cf35c94da
egorovmikhail <egorovmikhail@bk.ru>
2020-04-06 02:00:01 +1100

    Скруглил аватарки и добамил тень

---
+++ VK/VK/Avatar/AvatarShadowViev.swift
@@ -0,0 +1,22 @@
+//
+//  AvatarShadowViev.swift
+//  VK
+//
+//  Created by Михаил Егоров on 06.04.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import UIKit
+
+class AvatarShadowViev: UIView {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = bounds.height / 2
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 5
+        layer.shadowOffset = .zero
+    }
+}
---
+++ VK/VK/Avatar/AvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarView.swift
+//  VK
+//
+//  Created by Михаил Егоров on 05.04.2020.
+//  Copyright © 2020 Михаил Егоров. All rights reserved.
+//
+
+import UIKit
+
+class AvatarView: UIView {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = bounds.height / 2
+    }
+}
