f9e55813b69d318a5a67e933861ba17044e84196
egorovmikhail <egorovmikhail@bk.ru>
2020-05-11 00:51:50 +1100

    0

--- VK/VK/ News/NewsCell.swift
+++ VK/VK/ News/NewsCell.swift
@@ -17,16 +17,5 @@ class NewsCell: UITableViewCell {
     @IBOutlet weak var newsComment: UIView!
     @IBOutlet weak var newsShare: UIView!
     @IBOutlet weak var howManyViews: UIView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
