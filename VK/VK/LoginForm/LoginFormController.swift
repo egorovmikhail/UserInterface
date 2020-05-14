adbfbe09020ece6b289c44f7eba86f1d06e43870
egorovmikhail <egorovmikhail@bk.ru>
2020-05-15 03:04:11 +1100

    изменил Контроллер входа

--- VK/VK/LoginForm/LoginFormController.swift
+++ VK/VK/LoginForm/LoginFormController.swift
@@ -7,66 +7,69 @@
 //
 
 import UIKit
+import WebKit
 
 class LoginFormController: UIViewController {
     
-    @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
-
-    @IBOutlet weak var loginField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
-
+    
+    @IBOutlet weak var webview: WKWebView! {
+        
+        didSet{
+            webview.navigationDelegate = self
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWasShown(notification:)),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil)
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillBeHidden(notification:)),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil)
-    }
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        switch identifier {
-            case "loginSegue":
-                let isAuth = login()
-                if !isAuth {
-                    showErrorAlert()
-                }
-                return isAuth
-            default:
-                return true
-        }
-    }
-    
-    func login() -> Bool {
-        let login = loginField.text!
-        let password = passwordField.text!
-                
-        return login == "admin" && password == "123456"
-    }
-    
-    func showErrorAlert() {
-        let alert = UIAlertController(title: "Ошибка", message: "Неверный логин или пароль", preferredStyle: .alert)
-        let action = UIAlertAction(title: "ОК", style: .cancel, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
-    
-    @objc func keyboardWasShown(notification: Notification) {
-        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
-        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "7466169"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.68")
+        ]
         
-        scrollBottomConstraint.constant = frame.height
+        let request = URLRequest(url: urlComponents.url!)
+        
+        
+        webview.load(request)
     }
     
-    @objc func keyboardWillBeHidden(notification: Notification) {
-        scrollBottomConstraint.constant = 0
+    
+}
+
+extension LoginFormController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        let token = params["access_token"]
+        
+        print(token as Any)
+        
+        
+        decisionHandler(.cancel)
     }
 }
 
